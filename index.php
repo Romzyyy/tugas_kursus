0e1044a03922b52f6edf62ab3bf610a01eccadf8
romzyyy <fijay0117@gmail.com>
2022-12-14 01:57:58 +0700

    make a frontend for tampilan data siswa

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!doctype html>
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
+    <div class="container-fluid text-center mt-5 mb-5">
+        <h1>Data Siswa Sekolah</h1>
+    </div>
+    <div class="container-fluid w-75 position-relative mt-5">
+        <table class="table position-absolute top-0 start-50 translate-middle-x">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Kelas</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
